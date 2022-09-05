533474eee162153875b3b3fb44265037233fbc24
Tim <Tim.k.johnsen@gmail.com>
2022-09-05 16:55:29 -0700

    epsilon stable run
    couple minor debugs

--- observations/vector.py
+++ observations/vector.py
@@ -10,7 +10,8 @@ class Vector(Observation):
 			  names = None, 
 			  data_path = None,
 		   ):
-		super().__init__(_data=_data, data_path=data_path)
+		super().__init__(_data = _data, data_path = data_path)
+		self.names = names
 
 	# displays observation to console
 	def display(self):
@@ -18,16 +19,16 @@ class Vector(Observation):
 
 	# write as json
 	def write(self, 
-		   directory_path=None, 
-		   file_name=None,
+		   directory_path = None, 
+		   file_name = None,
 		   ):
 		file_path = super().write(
-			directory_path=directory_path, 
-			file_name=file_name,
+			directory_path = directory_path, 
+			file_name = file_name,
 			)
 		self.data_path = file_path + '.json'
 		# create dict from data
 		if self.names is None:
 			self.names = [str(idx) for idx in range(len(self._data))]
 		data_dict = {name:self._data[idx] for idx, name in enumerate(self.names)}
-		utils.write_json(data_dict, open(self.data_path, 'wb'))
+		utils.write_json(data_dict, self.data_path)
