d6f6877e3173b0137d5890706949ba65bad3fbd1
Tim <Tim.k.johnsen@gmail.com>
2023-01-25 19:47:37 -0800

    Update conda_env.bat

--- conda_env.bat
+++ conda_env.bat
@@ -1,41 +1,41 @@
-pip install --force-reinstall -v "cloudpickle==2.2.0"
-pip install --force-reinstall -v "pip install colorama==0.4.6"
-pip install --force-reinstall -v "contourpy==1.0.7"
-pip install --force-reinstall -v "cycler==0.11.0"
-pip install --force-reinstall -v "fonttools==4.38.0"
-pip install --force-reinstall -v "future==0.18.3"
-pip install --force-reinstall -v "imageio==2.24.0"
-pip install --force-reinstall -v "importlib-metadata==4.13.0"
-pip install --force-reinstall -v "kiwisolver==1.4.4"
-pip install --force-reinstall -v "msgpack-python==0.5.6"
-pip install --force-reinstall -v "networkx==3.0"
-pip install --force-reinstall -v "opencv-contrib-python==4.7.0.68"
-pip install --force-reinstall -v "packaging==23.0"
-pip install --force-reinstall -v "pandas==1.5.2"
-pip install --force-reinstall -v "Pillow==9.4.0"
-pip install --force-reinstall -v "py4j==0.10.9.7"
-pip install --force-reinstall -v "pyparsing==3.0.9"
-pip install --force-reinstall -v "python-dateutil==2.8.2"
-pip install --force-reinstall -v "pytz==2022.7.1"
-pip install --force-reinstall -v "PyWavelets==1.4.1"
-pip install --force-reinstall -v "scipy==1.10.0"
-pip install --force-reinstall -v "six==1.16.0"
-pip install --force-reinstall -v "tifffile==2022.10.10"
-pip install --force-reinstall -v "torch==1.13.1"
-pip install --force-reinstall -v "tornado==4.5.3"
-pip install --force-reinstall -v "tqdm==4.64.1"
-pip install --force-reinstall -v "typing_extensions==4.4.0"
-pip install --force-reinstall -v "wincertstore==0.2"
-pip install --force-reinstall -v "zipp==3.11.0"
-pip install numpy==1.24.1
-pip install hyperopt==0.2.7
-pip install binvox==0.1.5
-pip install --upgrade msgpack-rpc-python==0.4.1
-pip install airsim==1.8.1
-pip install psutil==5.9.4
-pip install scikit-image==0.19.3
-pip install matplotlib==3.6.3
-pip install gym==0.21.0
-pip install gym-notices==0.0.8
-pip install stable-baselines3==1.7.0
-pip install tensorboard==2.11.2
+python -m pip install --force-reinstall -v "cloudpickle==2.2.0"
+python -m pip install --force-reinstall -v "pip install colorama==0.4.6"
+python -m pip install --force-reinstall -v "contourpy==1.0.7"
+python -m pip install --force-reinstall -v "cycler==0.11.0"
+python -m pip install --force-reinstall -v "fonttools==4.38.0"
+python -m pip install --force-reinstall -v "future==0.18.3"
+python -m pip install --force-reinstall -v "imageio==2.24.0"
+python -m pip install --force-reinstall -v "importlib-metadata==4.13.0"
+python -m pip install --force-reinstall -v "kiwisolver==1.4.4"
+python -m pip install --force-reinstall -v "msgpack-python==0.5.6"
+python -m pip install --force-reinstall -v "networkx==3.0"
+python -m pip install --force-reinstall -v "opencv-contrib-python==4.7.0.68"
+python -m pip install --force-reinstall -v "packaging==23.0"
+python -m pip install --force-reinstall -v "pandas==1.5.2"
+python -m pip install --force-reinstall -v "Pillow==9.4.0"
+python -m pip install --force-reinstall -v "py4j==0.10.9.7"
+python -m pip install --force-reinstall -v "pyparsing==3.0.9"
+python -m pip install --force-reinstall -v "python-dateutil==2.8.2"
+python -m pip install --force-reinstall -v "pytz==2022.7.1"
+python -m pip install --force-reinstall -v "PyWavelets==1.4.1"
+python -m pip install --force-reinstall -v "scipy==1.10.0"
+python -m pip install --force-reinstall -v "six==1.16.0"
+python -m pip install --force-reinstall -v "tifffile==2022.10.10"
+python -m pip install --force-reinstall -v "torch==1.13.1"
+python -m pip install --force-reinstall -v "tornado==4.5.3"
+python -m pip install --force-reinstall -v "tqdm==4.64.1"
+python -m pip install --force-reinstall -v "typing_extensions==4.4.0"
+python -m pip install --force-reinstall -v "wincertstore==0.2"
+python -m pip install --force-reinstall -v "zipp==3.11.0"
+python -m pip install numpy==1.24.1
+python -m pip install hyperopt==0.2.7
+python -m pip install binvox==0.1.5
+python -m pip install --upgrade msgpack-rpc-python==0.4.1
+python -m pip install airsim==1.8.1
+python -m pip install psutil==5.9.4
+python -m pip install scikit-image==0.19.3
+python -m pip install matplotlib==3.6.3
+python -m pip install gym==0.21.0
+python -m pip install gym-notices==0.0.8
+python -m pip install stable-baselines3==1.7.0
+python -m pip install tensorboard==2.11.2
