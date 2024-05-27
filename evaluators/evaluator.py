83440ddda7338ae6ff5684e5678eec6180f94d14
Tim <Tim.k.johnsen@gmail.com>
2024-05-27 14:08:28 -0700

    Delete evaluators directory

--- evaluators/evaluator.py
+++
@@ -1,9 +0,0 @@
-# abstract class used to handle evaluators
-from component import Component
-
-class Evaluator(Component):
-	# constructor
-	def __init__(self):
-		pass
-	def connect(self, state=None):
-		super().connect(state)
