777a62bd6a66c6ba2e7812c24733ad2492176edc
WreckItTim <tim.k.johnsen@gmail.com>
2024-09-30 16:05:47 -0700

    pip fix

--- conda_env.bat
+++ conda_env.bat
@@ -1,47 +1,47 @@
-python -m pip install --force-reisntall -v "pip==21.0"
-python -m pip install --force-reinstall -v "cloudpickle==2.2.0"
-python -m pip install --force-reinstall -v "colorama==0.4.6"
-python -m pip install --force-reinstall -v "contourpy==1.0.7"
-python -m pip install --force-reinstall -v "cycler==0.11.0"
-python -m pip install --force-reinstall -v "fonttools==4.38.0"
-python -m pip install --force-reinstall -v "future==0.18.3"
-python -m pip install --force-reinstall -v "imageio==2.24.0"
-python -m pip install --force-reinstall -v "importlib-metadata==4.13.0"
-python -m pip install --force-reinstall -v "kiwisolver==1.4.4"
-python -m pip install --force-reinstall -v "msgpack-python==0.5.6"
-python -m pip install --force-reinstall -v "networkx==3.0"
-python -m pip install --force-reinstall -v "opencv-contrib-python==4.7.0.68"
-python -m pip install --force-reinstall -v "packaging==23.0"
-python -m pip install --force-reinstall -v "pandas==1.5.2"
-python -m pip install --force-reinstall -v "Pillow==9.4.0"
-python -m pip install --force-reinstall -v "py4j==0.10.9.7"
-python -m pip install --force-reinstall -v "pyparsing==3.0.9"
-python -m pip install --force-reinstall -v "python-dateutil==2.8.2"
-python -m pip install --force-reinstall -v "pytz==2022.7.1"
-python -m pip install --force-reinstall -v "PyWavelets==1.4.1"
-python -m pip install --force-reinstall -v "scipy==1.10.0"
-python -m pip install --force-reinstall -v "six==1.16.0"
-python -m pip install --force-reinstall -v "tifffile==2022.10.10"
-python -m pip install --force-reinstall -v "torch==1.13.1"
-python -m pip install --force-reinstall -v "tornado==4.5.3"
-python -m pip install --force-reinstall -v "tqdm==4.64.1"
-python -m pip install --force-reinstall -v "typing_extensions==4.4.0"
-python -m pip install --force-reinstall -v "wincertstore==0.2"
-python -m pip install --force-reinstall -v "zipp==3.11.0"
-python -m pip install numpy==1.24.1
-python -m pip install hyperopt==0.2.7
-python -m pip install binvox==0.1.5
+python -m pip install --upgrade pip==21.0
+python -m pip install --upgrade cloudpickle==2.2.0
+python -m pip install --upgrade colorama==0.4.6
+python -m pip install --upgrade contourpy==1.0.7
+python -m pip install --upgrade cycler==0.11.0
+python -m pip install --upgrade fonttools==4.38.0
+python -m pip install --upgrade future==0.18.3
+python -m pip install --upgrade imageio==2.24.0
+python -m pip install --upgrade importlib-metadata==4.13.0
+python -m pip install --upgrade kiwisolver==1.4.4
+python -m pip install --upgrade msgpack-python==0.5.6
+python -m pip install --upgrade networkx==3.0
+python -m pip install --upgrade opencv-contrib-python==4.7.0.68
+python -m pip install --upgrade packaging==23.0
+python -m pip install --upgrade pandas==1.5.2
+python -m pip install --upgrade Pillow==9.4.0
+python -m pip install --upgrade py4j==0.10.9.7
+python -m pip install --upgrade pyparsing==3.0.9
+python -m pip install --upgrade python-dateutil==2.8.2
+python -m pip install --upgrade pytz==2022.7.1
+python -m pip install --upgrade PyWavelets==1.4.1
+python -m pip install --upgrade scipy==1.10.0
+python -m pip install --upgrade six==1.16.0
+python -m pip install --upgrade tifffile==2022.10.10
+python -m pip install --upgrade torch==1.13.1
+python -m pip install --upgrade tornado==4.5.3
+python -m pip install --upgrade tqdm==4.64.1
+python -m pip install --upgrade typing_extensions==4.4.0
+python -m pip install --upgrade wincertstore==0.2
+python -m pip install --upgrade zipp==3.11.0
+python -m pip install --upgrade numpy==1.24.1
+python -m pip install --upgrade hyperopt==0.2.7
+python -m pip install --upgrade binvox==0.1.5
 python -m pip install --upgrade msgpack-rpc-python==0.4.1
-python -m pip install airsim==1.8.1
-python -m pip install psutil==5.9.4
-python -m pip install scikit-image==0.19.3
-python -m pip install matplotlib==3.6.3
-python -m pip install gym==0.21.0
-python -m pip install gym-notices==0.0.8
-python -m pip install setuptools==65.5.0
-python -m pip install stable-baselines3==1.7.0
-python -m pip install tensorboard==2.11.2
-python -m pip install nvidia-smi==0.1.3
-python -m pip install nvidia-ml-py3==7.352.0
-python -m pip install nvitop==1.0.0
-python -m pip install wandb==0.13.11
+python -m pip install --upgrade airsim==1.8.1
+python -m pip install --upgrade psutil==5.9.4
+python -m pip install --upgrade scikit-image==0.19.3
+python -m pip install --upgrade matplotlib==3.6.3
+python -m pip install --upgrade gym==0.21.0
+python -m pip install --upgrade gym-notices==0.0.8
+python -m pip install --upgrade setuptools==65.5.0
+python -m pip install --upgrade stable-baselines3==1.7.0
+python -m pip install --upgrade tensorboard==2.11.2
+python -m pip install --upgrade nvidia-smi==0.1.3
+python -m pip install --upgrade nvidia-ml-py3==7.352.0
+python -m pip install --upgrade nvitop==1.0.0
+python -m pip install --upgrade wandb==0.13.11
